3c2c60a9eeaa714af0322ceb78f50b4c2f168f9f
koundinya.abbaraju <koundinya.abbaraju@gmail.com>
2021-10-04 15:55:01 +0530

    Demo

---
+++ Mobiles.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+class Mobiles
+{
+	HashMap<String,ArrayList<String>> mobiles = new HashMap<>();
+	Mobiles(){}
+	public ArrayList<String>
+	{
+	}
+}
